68f8d78eafa875dd07a916bbb07ed4aa97f3a8de
ULC-Panoramics <robert.walter@unitedliving.co.uk>
2024-10-06 11:30:52 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ var viewer = pannellum.viewer('panorama', {
             "hfov":200,
             "yaw":210,
             "compass":true,
-            "northOffset":-210
+            "northOffset":0
         },
   
         "image-2": {
@@ -26,7 +26,7 @@ var viewer = pannellum.viewer('panorama', {
         "maxPitch": 25,
         "yaw": 210,
         "compass":true,
-        "northOffset":-210
+        "northOffset":0
       },
 
        "image-3": {
